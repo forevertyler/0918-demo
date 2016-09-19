dd0700d8192aca21799b8fb291b7aac42ffde028
forevertyler <1318719240@qq.com>
2016-09-19 14:17:07 +0800

    work

--- src/Work.js
+++ src/Work.js
@@ -4,13 +4,6 @@ import Card from './components/Card';
 import Bg from './image/home1.jpg';
 import { getJson } from './utils/help';
 
-let cardData = [
-  {title:'第一天',desc:'第一天描述',img:Bg,url:'a'},
-  {title:'第二天',desc:'第二天描述',img:Bg,url:'b'},
-  {title:'第三天',desc:'第三天描述',img:Bg,url:'c'},
-  {title:'第四天',desc:'第四天描述',img:Bg,url:'d'},
-  {title:'第五天',desc:'第五天描述',img:Bg,url:'e'}
-]
 
 class Work extends React.Component {
   constructor(){
